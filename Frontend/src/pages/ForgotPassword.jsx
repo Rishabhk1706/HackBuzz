af5b77655499be5c8fdb57f2ab7f2b5ca84babdf
Rishabhk1706 <rishabhk1706@gmail.com>
2026-07-28 22:53:34 +0530

    changed image format

--- Frontend/src/pages/ForgotPassword.jsx
+++ Frontend/src/pages/ForgotPassword.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import loginphoto from "../assets/loginphoto.png";
+import loginphoto from "../assets/loginphoto.webp";
 
 const ForgotPassword = () => {
   const navigate = useNavigate();
